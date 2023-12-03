45b3c4c18f25823b5749cff35b885397c7869fee
Keks <keks@htmlacademy.ru>
2023-12-03 20:02:29 +0300

    :heavy_check_mark: Сборка #9

---
+++ 9/js/modules/util.js
@@ -0,0 +1,24 @@
+const getRandomNumber = (min, max) => {
+  const leftBorder = Math.ceil(Math.min(Math.abs(min), Math.abs(max)));
+  const rightBorder = Math.floor(Math.max(Math.abs(min), Math.abs(max)));
+  const randomNumber = Math.random() * (rightBorder-leftBorder+1) + leftBorder;
+  return Math.floor(randomNumber);
+};
+
+const getRandomIdGenerator = (min, max) => {
+  const previousValues = [];
+
+  return function() {
+    let currentValue = getRandomNumber(min, max);
+    while(previousValues.includes(currentValue)){
+      currentValue = getRandomNumber(min, max);
+    }
+    previousValues.push(currentValue);
+    return currentValue;
+  };
+};
+
+const getRandomElement = (array) => array[getRandomNumber(0, array.length-1)];
+const isEscapeKey = (evt) => evt.key === 'Escape';
+
+export {getRandomNumber, getRandomElement, getRandomIdGenerator, isEscapeKey};
